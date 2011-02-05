54c358ccfd85c951af4b3ea3f39b50c227eaf619
Ludo van den Boom <ludo@cubicphuse.nl>
2011-02-05 16:34:11 +0100

    I want a single dash per level, and a space after each element's 'text'. Hooray for forking.

--- lib/tree_select.rb
+++ lib/tree_select.rb
@@ -7,33 +7,34 @@ module ActionView
         select_deselect = {}
         select_deselect[:selected] = extract_values_from_collection(collection, value_method, selected)
         select_deselect[:disabled] = extract_values_from_collection(collection, value_method, disabled)
-        
+
         options_for_select(options, select_deselect)
       end
 
       def tree_select(object, method, collection, value_method, text_method, options = {}, html_options = {})
         InstanceTag.new(object, method, self, options.delete(:object)).to_tree_select_tag(collection, value_method, text_method, options, html_options)
       end
-      
-      private
+
+    private
+
       def recurse_options_from_tree_collection_for_select(collection, value_method, text_method, level = 0)
-        prefix = '-' * 2 * level
+        prefix = "-" * level
         result = []
         collection.each do |element|
-          result << ["#{prefix}#{element.send(text_method)}", element.send(value_method)]    
-          if element.children.size > 0
-            result = result + recurse_options_from_tree_collection_for_select(element.children, value_method, text_method, level + 1)
+          result << ["#{prefix} #{element.send(text_method)}", element.send(value_method)]
+
+          unless element.children.empty?
+            result = result + recurse_options_from_tree_collection_for_select(element.children, value_method, text_method, level+1)
           end
         end
-        
+
         result
       end
     end
 
-  
     class InstanceTag
       include FormOptionsHelper
-    
+
       def to_tree_select_tag(collection, value_method, text_method, options, html_options)
         html_options = html_options.stringify_keys
         add_default_name_and_id(html_options)
@@ -45,7 +46,7 @@ module ActionView
         )
       end
     end
-    
+
     class FormBuilder
       def tree_select(method, collection, value_method, text_method, options = {}, html_options = {})
         @template.tree_select(@object_name, method, collection, value_method, text_method, objectify_options(options), @default_options.merge(html_options))
